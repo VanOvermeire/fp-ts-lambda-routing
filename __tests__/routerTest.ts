7639d6d3f0bcb7b12f13a6017c397486a7fe825b
Sam Van Overmeire <svanovermeire@branch.be>
2021-08-03 21:30:21 +0200

    WIP

--- __tests__/routerTest.ts
+++ __tests__/routerTest.ts
@@ -1,10 +1,11 @@
 import * as TE from 'fp-ts/TaskEither';
 import * as T from 'fp-ts/Task';
-import {router} from "../src/router";
 import {APIGatewayEvent} from "aws-lambda";
 import {pipe} from "fp-ts/function";
+import {router} from "../src";
 
 const fooFunction = () => TE.right('Foo');
+const fooAltFunction = () => TE.right('FooAlt');
 const barFunction = () => TE.left('Bar');
 
 const unWrapper = <R, S>(t: TE.TaskEither<R, S>) =>
@@ -49,8 +50,59 @@ describe('router tests',  () => {
         expect(result).toEqual('Foo');
     });
 
-    // TODO more complex paths
-    //  -> /foo/* = anything starting with foo
-    //  prefer an exact match
-    //  what about multiple matches? should that be allowed? i.e. /foo/* versus /foo/more/specific/*
+    test('should call right function when path ends with wildcard', async () => {
+        const dict = {
+            '/foo/*': fooFunction,
+            '/bar': barFunction,
+        };
+        const exampleEvent = {
+            path: '/foo',
+        } as APIGatewayEvent;
+
+        const routerForDict = router(dict, String);
+
+        const result = await unWrapper(
+            routerForDict(exampleEvent)
+        );
+
+        expect(result).toEqual('Foo');
+    });
+
+    test('should prefer an exact match', async () => {
+        const dict = {
+            '/foo/*': fooAltFunction,
+            '/foo': fooFunction,
+            '/bar': barFunction,
+        };
+        const exampleEvent = {
+            path: '/foo',
+        } as APIGatewayEvent;
+
+        const routerForDict = router(dict, String);
+
+        const result = await unWrapper(
+            routerForDict(exampleEvent)
+        );
+
+        expect(result).toEqual('Foo');
+    });
+
+    test('should prefer a longer path', async () => {
+        const dict = {
+            '/foo/*': fooAltFunction,
+            '/foo/more/specific/*': fooFunction,
+            '/bar': barFunction,
+        };
+        const exampleEvent = {
+            path: '/foo',
+        } as APIGatewayEvent;
+
+        const routerForDict = router(dict, String);
+
+        const result = await unWrapper(
+            routerForDict(exampleEvent)
+        );
+
+        expect(result).toEqual('Foo');
+    });
 });
--- src/router.ts
+++ src/router.ts
@@ -3,14 +3,48 @@ import type {APIGatewayEvent} from "aws-lambda";
 import * as TE from 'fp-ts/TaskEither';
 
 // TODO also offer Either? further abstract concrete monad type?
+// TODO other locations for lambda path?
 
 export type RouterFunction<T, R> = (e: APIGatewayEvent) => TaskEither<T, R>;
 
-const internalRouter = <T, R>(endpoints: Record<string, RouterFunction<T, R>>, errorConstructor: (err: string) => T) => (event: APIGatewayEvent): TaskEither<T, R> => {
-    const path = event.path;
-    const rightEndpoint = endpoints[path];
+const matchRoute = (path: string) => (route: string) => {
+    const routeWithForwardSlash = route.startsWith("/") ? route : `/${route}`;
+    const pathWithForwardSlash = path.startsWith("/") ? route : `/${path}`;
 
-    return rightEndpoint ? rightEndpoint(event) : TE.left(errorConstructor(`No route found for path ${path}`));
+    if (routeWithForwardSlash.endsWith("*")) {
+        return pathWithForwardSlash.startsWith(routeWithForwardSlash.replace("*", ""));
+    }
+    return routeWithForwardSlash === path;
+}
+
+const findApp = <T, R>(path: string) => (endpoints: Record<string, RouterFunction<T, R>>): RouterFunction<T, R> | undefined => {
+    const matchRouteForPath = matchRoute(path);
+
+    Object.entries(endpoints)
+        .filter(routeMapping => matchRouteForPath(routeMapping[0]))
+        .reduce((acc: RouterFunction<T, R>[], curr) => {
+            if(acc.length === 0) {
+                acc.push(curr[1]);
+            }
+
+            return acc;
+        }, [])
+        // .map(routeMapping => routeMapping[1])
+        .shift();
+
+    return Object.entries(endpoints)
+        .filter(routeMapping => matchRouteForPath(routeMapping[0]))
+        .map(routeMapping => routeMapping[1])
+        .shift();
+    // instead: reduce
+}
+
+
+const internalRouter = <T, R>(endpoints: Record<string, RouterFunction<T, R>>, errorConstructor: (err: string) => T) => (event: APIGatewayEvent): TaskEither<T, R> => {
+    const path = event.path ?? 'undefined';
+    const rightEndpoint = findApp(path)(endpoints);
+
+    return rightEndpoint ? rightEndpoint(event) as TaskEither<T, R> : TE.left(errorConstructor(`No route found for path ${path}`)); // is this assertion necessary?
 };
 
 export function router<T, R>(endpoints: Record<string, RouterFunction<T, R>>, errorConstructor: (err: string) => T): (event: APIGatewayEvent) => TaskEither<T, R>;
